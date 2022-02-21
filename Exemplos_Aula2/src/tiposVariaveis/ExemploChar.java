35ed776b1dc4a15619b0c1b50a3bceae1979c08e
Michelle Hanne Soares de Andrade <michelle.hanne@ormuztech.com.br>
2022-02-21 15:31:57 -0300

    Exemplo_aula2

---
+++ Exemplos_Aula2/src/tiposVariaveis/ExemploChar.java
@@ -0,0 +1,17 @@
+package tiposVariaveis;
+
+public class ExemploChar {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		char ch;
+		ch = 'X';
+		System.out.println("ch contains " + ch);
+		ch++; // incrementa ch
+		System.out.println("ch is now " + ch);
+		ch = 90; // da a ch o valor Z	
+		System.out.println("ch is now " + ch);	
+		
+	}
+
+}
